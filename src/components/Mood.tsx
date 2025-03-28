4cdcc7d00fd7ae92862ff2cb00387840109d40b5
Arthur Vincent Simon <developarvin@gmail.com>
2025-03-28 08:25:04 +0800

    Refactor Mood component to use button elements and enhance click handling; update layout for improved spacing

--- src/components/Mood.tsx
+++ src/components/Mood.tsx
@@ -1,14 +1,22 @@
-interface MoodProps extends React.HTMLAttributes<HTMLDivElement> {
+import { useCallback } from "react";
+
+interface MoodProps extends React.HTMLAttributes<HTMLButtonElement> {
 	children?: React.ReactNode;
 }
 
 export const MoodItem: React.FC<MoodProps> = ({ children, color, ...rest }) => {
+	const onClickHandler = useCallback(() => {
+		console.info(`MoodItem clicked: ${color}`);
+	}, [color]);
+
 	return (
-		<div
+		<button
+			type="button"
 			className={`${color} rounded-full h-24 w-24 flex items-center justify-center text-white font-medium cursor-pointer transition-transform hover:scale-105 text-sm`}
 			{...rest}
+			onClick={onClickHandler}
 		>
 			{children}
-		</div>
+		</button>
 	);
 };
--- src/routes/mood.tsx
+++ src/routes/mood.tsx
@@ -5,135 +5,137 @@ export const Route = createFileRoute("/mood")({
 	component: Mood,
 });
 
-function Mood() {
-	const moods = [
-		{
-			id: 1,
-			name: "Happy",
-			color: "bg-yellow-500",
-		},
-		{
-			id: 2,
-			name: "Sad",
-			color: "bg-blue-500",
-		},
-		{
-			id: 3,
-			name: "Angry",
-			color: "bg-red-500",
-		},
-		{
-			id: 4,
-			name: "Tired",
-			color: "bg-gray-500",
-		},
-		{
-			id: 5,
-			name: "Excited",
-			color: "bg-green-500",
-		},
-		{
-			id: 6,
-			name: "Anxious",
-			color: "bg-purple-500",
-		},
-		{
-			id: 7,
-			name: "Calm",
-			color: "bg-teal-500",
-		},
-		{
-			id: 8,
-			name: "Confused",
-			color: "bg-indigo-500",
-		},
-		{
-			id: 9,
-			name: "Frustrated",
-			color: "bg-pink-500",
-		},
-		{
-			id: 10,
-			name: "Grateful",
-			color: "bg-amber-500",
-		},
-		{
-			id: 11,
-			name: "Hopeful",
-			color: "bg-lime-500",
-		},
-		{
-			id: 12,
-			name: "Inspired",
-			color: "bg-emerald-500",
-		},
-		{
-			id: 13,
-			name: "Lonely",
-			color: "bg-violet-500",
-		},
-		{
-			id: 14,
-			name: "Loved",
-			color: "bg-rose-500",
-		},
-		{
-			id: 15,
-			name: "Nervous",
-			color: "bg-cyan-500",
-		},
-		{
-			id: 16,
-			name: "Overwhelmed",
-			color: "bg-amber-500",
-		},
-		{
-			id: 17,
-			name: "Peaceful",
-			color: "bg-lime-500",
-		},
-		{
-			id: 18,
-			name: "Relaxed",
-			color: "bg-emerald-500",
-		},
-		{
-			id: 19,
-			name: "Stressed",
-			color: "bg-violet-500",
-		},
-		{
-			id: 20,
-			name: "Surprised",
-			color: "bg-rose-500",
-		},
-		{
-			id: 21,
-			name: "Worried",
-			color: "bg-cyan-500",
-		},
-		{
-			id: 22,
-			name: "Other",
-			color: "bg-gray-500",
-		},
-	];
+const moods = [
+	{
+		id: 1,
+		name: "Happy",
+		color: "bg-yellow-500",
+	},
+	{
+		id: 2,
+		name: "Sad",
+		color: "bg-blue-500",
+	},
+	{
+		id: 3,
+		name: "Angry",
+		color: "bg-red-500",
+	},
+	{
+		id: 4,
+		name: "Tired",
+		color: "bg-gray-500",
+	},
+	{
+		id: 5,
+		name: "Excited",
+		color: "bg-green-500",
+	},
+	{
+		id: 6,
+		name: "Anxious",
+		color: "bg-purple-500",
+	},
+	{
+		id: 7,
+		name: "Calm",
+		color: "bg-teal-500",
+	},
+	{
+		id: 8,
+		name: "Confused",
+		color: "bg-indigo-500",
+	},
+	{
+		id: 9,
+		name: "Frustrated",
+		color: "bg-pink-500",
+	},
+	{
+		id: 10,
+		name: "Grateful",
+		color: "bg-amber-500",
+	},
+	{
+		id: 11,
+		name: "Hopeful",
+		color: "bg-lime-500",
+	},
+	{
+		id: 12,
+		name: "Inspired",
+		color: "bg-emerald-500",
+	},
+	{
+		id: 13,
+		name: "Lonely",
+		color: "bg-violet-500",
+	},
+	{
+		id: 14,
+		name: "Loved",
+		color: "bg-rose-500",
+	},
+	{
+		id: 15,
+		name: "Nervous",
+		color: "bg-cyan-500",
+	},
+	{
+		id: 16,
+		name: "Overwhelmed",
+		color: "bg-amber-500",
+	},
+	{
+		id: 17,
+		name: "Peaceful",
+		color: "bg-lime-500",
+	},
+	{
+		id: 18,
+		name: "Relaxed",
+		color: "bg-emerald-500",
+	},
+	{
+		id: 19,
+		name: "Stressed",
+		color: "bg-violet-500",
+	},
+	{
+		id: 20,
+		name: "Surprised",
+		color: "bg-rose-500",
+	},
+	{
+		id: 21,
+		name: "Worried",
+		color: "bg-cyan-500",
+	},
+	{
+		id: 22,
+		name: "Other",
+		color: "bg-gray-500",
+	},
+];
 
+function Mood() {
 	return (
 		<>
 			{/* Question */}
-			<div className="text-center text-lg font-semibold mb-4">
+			<div className="text-center text-2xl font-semibold mb-4 py-8">
 				How are you feeling today?
 			</div>
 
-			{/* Circle Area */}
-			<div className="flex items-center justify-center">
-				<div className="grid grid-cols-3 gap-4">
-					{moods.map((mood) => (
-						<MoodItem key={mood.id} color={mood.color}>
-							{mood.name}
-						</MoodItem>
-					))}
+			{/* Moods */}
+			<div className="flex items-center justify-center h-[64vh]">
+				<div className="overflow-y-auto max-h-full w-full px-4 pb-4">
+					<div className="grid grid-cols-3 gap-4">
+						{moods.map((mood) => (
+							<MoodItem key={mood.id} color={mood.color}>
+								{mood.name}
+							</MoodItem>
+						))}
+					</div>
 				</div>
 			</div>
 		</>
